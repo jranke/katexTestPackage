f5e0acfd4ef73d6fe1796255d74987b5f6294ccd
Johannes Ranke <jranke@uni-bremen.de>
2023-05-19 06:37:24 +0200

    Simplified package without math

--- R/hello.R
+++ R/hello.R
@@ -1,5 +1,13 @@
-
-## a placeholder
+##' A placeholder function
+##'
+##' This function shows a standard text on the console. In a time-honoured
+##' tradition, it defaults to displaying \emph{hello, world}.
+##' @param txt An optional character variable, defaults to \sQuote{world}
+##' @return Nothing is returned but as a side effect output is printed
+##' @examples
+##' hello()
+##' hello("and goodbye")
+##' @export
 hello <- function(txt = "world") {
     cat("Hello, ", txt, "\n")
 }
